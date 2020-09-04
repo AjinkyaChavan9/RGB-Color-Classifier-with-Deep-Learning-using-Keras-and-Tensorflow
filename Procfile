43156a659de339f54ead80d2aa49763fe1ec51c5
Ajinkya Chavan <46496851+AjinkyaChavan9@users.noreply.github.com>
2020-09-04 21:49:20 +0530

    heroku files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: sh setup.sh && streamlit run RGB_web_app.py
---
+++ setup.sh
@@ -0,0 +1,8 @@
+mkdir -p ~/.streamlit
+
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
+
